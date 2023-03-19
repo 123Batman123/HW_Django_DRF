e9d6390c0f5e3264cccd07a5b126196a152956dd
Zakhar Kuznetsov <Z.kuz1994@yandex.ru>
2023-03-19 17:26:32 +0300

    Initial commit

---
+++ measurement/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Sensor, Measurement
+
+class MeasurementSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Measurement
+        fields = ['temperature', 'date', 'id_sensor']
+
+
+class MeasurementSerializer_2(serializers.ModelSerializer):
+
+    class Meta:
+        model = Measurement
+        fields = ['temperature', 'date']
+
+
+class SensorDetailSerializer(serializers.ModelSerializer):
+
+    measurement = MeasurementSerializer_2(read_only=True, many=True)
+
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description', 'measurement']
+        depth = 1
+
